b9b4de73d2b42cac15eb165d30f4aa92c528c763
Максим Гулько <gulashmail@gmail.com>
2025-01-02 23:48:51 +0300

    working state

---
+++ spring-boots/webflux-prj/src/main/java/com/gulash/example/webfluxprj/manual_run/flux/init/GenerateExample.java
@@ -0,0 +1,30 @@
+package com.gulash.example.webfluxprj.manual_run.flux.init;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.SynchronousSink;
+
+public class GenerateExample {
+    public static void main(String[] args) {
+        // todo Flux.generate позволяет СИНХРОННО генерировать элементы по одному с использованием состояния(SynchronousSink)
+        Flux<Integer> generate = Flux.generate(
+            () -> 0, // initial state
+            (Integer currentState, SynchronousSink<Integer> synchronousSink) -> {
+                // todo что-то отдали в поток
+                synchronousSink.next(currentState);
+
+                // Exception
+                // synchronousSink.error(new RuntimeException("something went wrong"));
+
+                // todo complete - завершаемся когда нужно
+                if (currentState == 10) {
+                    synchronousSink.complete();
+                }
+                // todo передали на следующую итерацию
+                return currentState + 1;
+            }
+        );
+
+        generate
+            .subscribe(System.out::println);
+    }
+}
