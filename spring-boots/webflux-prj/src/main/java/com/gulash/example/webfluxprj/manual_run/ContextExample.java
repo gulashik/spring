e581b6fe93d852311b5880419a103de76e9bf45d
Максим Гулько <gulashmail@gmail.com>
2025-01-06 00:09:19 +0300

    working state

---
+++ spring-boots/webflux-prj/src/main/java/com/gulash/example/webfluxprj/manual_run/ContextExample.java
@@ -0,0 +1,74 @@
+package com.gulash.example.webfluxprj.manual_run;
+
+import reactor.core.Disposable;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Hooks;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+import reactor.util.context.Context;
+
+import java.util.List;
+
+public class ContextExample {
+    public static void main(String[] args) {
+        // todo Context — это неизменяемая MAP-а, которую можно использовать для передачи данных(идентификаторы запросов, токены или параметры) по цепочке реактивного программирования.
+
+        Disposable disposable = Flux.range(1, 5)
+            // todo Получаем на вход read-only контекст
+            .transformDeferredContextual(
+                (integerFlux, contextView) -> {
+                    // todo получаем из контекста
+                    String contextWriteVar = contextView.getOrDefault("contextWrite_key", "defWrite_value");
+                    System.out.println("contextWriteVar: " + contextWriteVar);
+
+                    // todo проверяем есть ли ключ
+                    if (contextView.hasKey("contextWrite_key")) {
+                        String someKey = contextView.get("contextWrite_key");
+                        System.out.println("contextWrite_key: " + someKey);
+                    }
+
+                    // todo выводим всё что есть в контексте
+                    contextView.forEach((key, value) -> System.out.println("key: " + key + " value: " + value));
+
+                    return integerFlux;
+                }
+            )
+            .map(
+                integer -> {
+                    // todo обращаемся к контексту через deferContextual
+                    Flux<String> stringFlux = Flux.deferContextual(
+                        contextView -> {
+                            String contextWriteVar = contextView.getOrDefault("contextWrite_key", "defWrite_value");
+                            return Flux.just(contextWriteVar);
+                        }
+                    );
+                    stringFlux
+                        .subscribeOn(Schedulers.boundedElastic())
+                        .subscribe(value -> System.out.println("Выводим через Mono.deferContextual: " + value));
+
+                    return integer;
+                }
+            )
+            // todo добавляем в контекст. Похоже это нужно делать ниже чтения из контекста
+            .contextWrite(Context.of("contextWrite_key", "contextWrite_value"))
+
+            .subscribeOn(Schedulers.boundedElastic())
+            .subscribe(
+                value -> System.out.println(value),
+                error -> System.err.println(error),
+                () -> System.out.println("Done"),
+                // todo создаём начальный контекст
+                Context.of("subscr_key", "subscr_value")
+            );
+
+          waitForDisposableEnd(List.of(disposable));
+    }
+    private static void waitForDisposableEnd(List<Disposable> disposableList) {
+        disposableList.forEach(
+            // isDisposed
+            //  true, если ресурс был освобожден (закрыт или отменен).
+            //  false, если ресурс все еще активен.
+            disposable -> { while (!disposable.isDisposed()) {}}
+        );
+    }
+}
