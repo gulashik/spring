91e10182bc2e1d9bafcbe1c50ea8e387615dba95
Максим Гулько <gulashmail@gmail.com>
2025-01-06 21:41:27 +0300

    working state

---
+++ spring-boots/webflux-prj/src/main/java/com/gulash/example/webfluxprj/manual_run/OnBackpressureBufferExample.java
@@ -0,0 +1,59 @@
+package com.gulash.example.webfluxprj.manual_run;
+
+import reactor.core.Disposable;
+import reactor.core.publisher.BufferOverflowStrategy;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+public class OnBackpressureBufferExample {
+    public static void main(String[] args) {
+        // todo onBackpressureBuffer - настройки хранения событий,для управления при избыточном количестве не обработаных событий
+
+        //exampleWithTtll();
+        exampleWithStrategy();
+    }
+
+    private static void exampleWithStrategy() {
+        Disposable disposable = Flux.range(1, 100)
+            .log()
+            .onBackpressureBuffer(
+                3, // todo храним максимум
+                integer -> System.out.println("Dropped - " + integer), // todo Обработка переполнения
+                BufferOverflowStrategy.DROP_LATEST // todo стратегия переполнения
+            )
+            .delayElements(Duration.of(10, ChronoUnit.SECONDS))
+            .subscribeOn(Schedulers.boundedElastic())
+            .subscribe(System.out::println);
+
+        waitForDisposableEnd(List.of(disposable));
+    }
+    private static void exampleWithTtll() {
+        Disposable disposable = Flux.range(1, 10)
+            .log()
+            .onBackpressureBuffer(
+                Duration.ofSeconds(3), // todo ttl время жизни события
+                3, // todo храним максимум
+                integer -> System.out.println("Dropped - " + integer), // todo Обработка переполнения
+                Schedulers.boundedElastic() // todo планировщик потоков
+            )
+            .delayElements(Duration.of(10, ChronoUnit.SECONDS))
+            .subscribeOn(Schedulers.boundedElastic())
+            .subscribe(System.out::println);
+
+        waitForDisposableEnd(List.of(disposable));
+    }
+
+    private static void waitForDisposableEnd(List<Disposable> disposableList) {
+        disposableList.forEach(
+            // isDisposed
+            //  true, если ресурс был освобожден (закрыт или отменен).
+            //  false, если ресурс все еще активен.
+            disposable -> { while (!disposable.isDisposed()) {}}
+        );
+    }
+
+}
