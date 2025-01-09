35703fd4e66f7f44938b31219737ecc09c23326a
Максим Гулько <gulashmail@gmail.com>
2025-01-09 21:35:22 +0300

    working state

--- spring-boots/webflux-prj/src/main/java/com/gulash/example/webfluxprj/manual_run/flux/ParallelAndSequenceExample.java
+++ spring-boots/webflux-prj/src/main/java/com/gulash/example/webfluxprj/manual_run/flux/ParallelAndSequenceExample.java
@@ -1,13 +1,11 @@
 package com.gulash.example.webfluxprj.manual_run.flux;
 
-import lombok.extern.slf4j.Slf4j;
 import reactor.core.Disposable;
 import reactor.core.publisher.Flux;
 import reactor.core.scheduler.Schedulers;
 
 import java.util.List;
 
-@Slf4j
 public class ParallelAndSequenceExample {
 
     public static void main(String[] args) throws InterruptedException {
@@ -19,10 +17,17 @@ public class ParallelAndSequenceExample {
     private static void parallelAndSequential() {
 
         Disposable disposable = Flux.range(1, 10)
-            .parallel(4) // Перевод Flux в параллельный режим
-            .runOn(Schedulers.parallel()) // Выполнение на общем параллельном Scheduler
-            .map(i -> i * 2)
-            .sequential() // Возвращение в последовательный режим
+            .log()
+            // todo Параллелим поток в несколько параллельных потоков
+            .parallel(4) .runOn(Schedulers.parallel())
+            .map(
+                i -> {
+                    System.out.println("Processing " + i + " on thread: " + Thread.currentThread().getName());
+                    return i;
+                }
+            )
+            // todo Возвращение потока в последовательный режим
+            .sequential()
             .subscribe(System.out::println);
 
         waitForDisposableEnd(List.of(disposable));
