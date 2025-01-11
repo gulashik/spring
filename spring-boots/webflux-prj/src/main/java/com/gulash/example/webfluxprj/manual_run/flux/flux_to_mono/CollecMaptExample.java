1ddfc5fa067de407c5113e47135adbbabccf9908
Максим Гулько <gulashmail@gmail.com>
2025-01-11 16:43:18 +0300

    working state

--- spring-boots/webflux-prj/src/main/java/com/gulash/example/webfluxprj/manual_run/flux/flux_to_mono/CollecMaptExample.java
+++ spring-boots/webflux-prj/src/main/java/com/gulash/example/webfluxprj/manual_run/flux/flux_to_mono/CollecMaptExample.java
@@ -1,15 +1,12 @@
-package com.gulash.example.webfluxprj.manual_run.flux;
+package com.gulash.example.webfluxprj.manual_run.flux.flux_to_mono;
 
 import reactor.core.Disposable;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.util.Collection;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
-public class CollecMiltyMaptExample {
+public class CollecMaptExample {
     public static void main(String[] args) {
         // todo предварительно запуск spring-boots/webflux-prj/compose.md
 
@@ -19,13 +16,13 @@ public class CollecMiltyMaptExample {
     private static void example() {
         Flux<String> words = Flux.just("hello", "world", "hello", "hello", "hello", "hello");
 
-        // todo collectMultimap - позволяет собирать элементы потока в Map. КОЛЛИЗИИ ПО КЛЮЧАМ в Коллекцию.
-        Mono<Map<String, Collection<String>>> mapMono = words
-            .collectMultimap(
+        // todo collectMap - позволяет собирать элементы потока в Map. НЕТ ОБРАБОТКИ КОЛЛИЗИИ ПО КЛЮЧАМ. Используем collect, collectMultimap
+        Mono<Map<String, String>> mapMono = words
+            .collectMap(
                 s -> s, /*key*/
-                s -> s, /*Collection<value>*/
+                s -> s, /*value*/
                 LinkedHashMap::new /*можно указать конкретный тип*/
-            );// {hello=[hello, hello, hello, hello, hello], world=[world]}
+            );// {hello=hello, world=world}
 
         mapMono.subscribe(System.out::println);
 
--- spring-boots/webflux-prj/src/main/java/com/gulash/example/webfluxprj/manual_run/flux/flux_to_mono/CollectListExample.java
+++ spring-boots/webflux-prj/src/main/java/com/gulash/example/webfluxprj/manual_run/flux/flux_to_mono/CollectListExample.java
@@ -1,4 +1,4 @@
-package com.gulash.example.webfluxprj.manual_run.flux;
+package com.gulash.example.webfluxprj.manual_run.flux.flux_to_mono;
 
 import reactor.core.Disposable;
 import reactor.core.publisher.Flux;
