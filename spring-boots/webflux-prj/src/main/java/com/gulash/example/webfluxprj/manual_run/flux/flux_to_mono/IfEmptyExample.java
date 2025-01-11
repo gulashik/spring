2025901378321e67679aa32fc0900c44b4e47779
Максим Гулько <gulashmail@gmail.com>
2025-01-11 18:13:23 +0300

    working state

---
+++ spring-boots/webflux-prj/src/main/java/com/gulash/example/webfluxprj/manual_run/flux/flux_to_mono/IfEmptyExample.java
@@ -0,0 +1,41 @@
+package com.gulash.example.webfluxprj.manual_run.flux.flux_to_mono;
+
+import reactor.core.Disposable;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+public class IfEmptyExample {
+    public static void main(String[] args) {
+        // todo предварительно запуск spring-boots/webflux-prj/compose.md
+        ifEmpty();
+    }
+
+    private static void ifEmpty() {
+
+        Flux<String> emptyStringFlux = Flux.empty();
+
+        Flux<String> stringFlux1 = emptyStringFlux
+            // todo defaultIfEmpty - указываем ЗНАЧЕНИЕ если поток пустой
+            .defaultIfEmpty("default value from literal");
+
+        stringFlux1.subscribe(System.out::println);
+
+        Flux<String> stringFlux2 = emptyStringFlux
+            // todo switchIfEmpty - указываем PUBLISHER если поток пустой
+            .switchIfEmpty(Mono.just("default value from Publisher"));
+
+        stringFlux2.subscribe(System.out::println);
+    }
+
+    // ожидалка окончания Disposable
+    private static void waitForDisposableEnd(List<Disposable> disposableList) {
+        disposableList.forEach(
+            // isDisposed
+            //  true, если ресурс был освобожден (закрыт или отменен).
+            //  false, если ресурс все еще активен.
+            disposable -> { while (!disposable.isDisposed()) {}}
+        );
+    }
+}
