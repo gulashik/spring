4c8555b7a6a2d9af756c9252b9632b24d05b5df2
Максим Гулько <gulashmail@gmail.com>
2025-02-22 00:41:16 +0300

    working state

--- spring-boots/springboot-security/ss-prj/README.md
+++ spring-boots/springboot-security/ss-prj/README.md
@@ -2,3 +2,6 @@
 
 зависимости</br>
 [pom.xml](pom.xml)
+
+config class @Configuration + @EnableWebSecurity + Beans</br>
+[SecurityConfiguration.java](src%2Fmain%2Fjava%2Fru%2Fotus%2Fspring%2Fsecurity%2FSecurityConfiguration.java)
--- spring-boots/springboot-security/ss-prj/src/main/java/ru/otus/spring/security/SecurityConfiguration.java
+++ spring-boots/springboot-security/ss-prj/src/main/java/ru/otus/spring/security/SecurityConfiguration.java
@@ -14,22 +14,32 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+
+// todo указывает Spring, что класс содержит конфигурацию безопасности для веб-приложения.
+//  при использовании @EnableWebSecurity стандартная конфигурация отключается
 @EnableWebSecurity
 public class SecurityConfiguration {
 
+    // todo Нужно реализовать SecurityFilterChain bean
+    // todo SecurityFilterChain(часть механизма фильтров Servlet API) — это цепочка фильтров, которая управляет
+    //  процессом обработки всех HTTP-запросов и их защитой (Аутентификация, Авторизация, Обработка запросов/ответов).
     @Bean
-    public SecurityFilterChain securityFilterChain( HttpSecurity http ) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests( ( authorize ) -> authorize
-                        .requestMatchers(  "/public" ).permitAll()
-                        .requestMatchers( "/authenticated" ).authenticated()
-                        .anyRequest().permitAll()
-                )
-                .httpBasic(Customizer.withDefaults());
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(
+                (authorize) -> authorize
+                    .requestMatchers("/public").permitAll() // Доступно всем
+                    // .requestMatchers("/admin/**").hasRole("ADMIN") // Только для администраторов
+                    .requestMatchers("/authenticated").authenticated()
+
+                    // todo anyRequest - является "заглушкой" для всех запросов, которые не были явно настроены ранее
+                    .anyRequest().permitAll() // permitAll()- Все остальные запросы доступны всем
+                    //.anyRequest().authenticated() // authenticated() - Все остальные запросы требуют аутентификации
+            )
+            .httpBasic(Customizer.withDefaults());
         return http.build();
     }
-
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -41,11 +51,11 @@ public class SecurityConfiguration {
     @Bean
     public InMemoryUserDetailsManager userDetailsService() {
         UserDetails user = User
-                .builder()
-                .username("user")
-                .password("password")
-                .roles("USER")
-                .build();
+            .builder()
+            .username("user")
+            .password("password")
+            .roles("USER")
+            .build();
         return new InMemoryUserDetailsManager(user);
     }
 }
