5e4f74d70eec3dad9dcc57230dccf047ea890b4b
Максим Гулько <gulashmail@gmail.com>
2025-05-18 17:33:04 +0300

    working state

--- spring-boots/spring-micrometer/micrometer-demo/src/main/java/org/gualsh/demo/micromet/MicrometerDemoApplication.java
+++ spring-boots/spring-micrometer/micrometer-demo/src/main/java/org/gualsh/demo/micromet/MicrometerDemoApplication.java
@@ -5,26 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-/**
- * Основной класс приложения, демонстрирующего возможности Micrometer.
- * <p>
- * Данное приложение показывает различные способы использования Micrometer для
- * мониторинга приложения Spring Boot, включая встроенные метрики, создание
- * пользовательских метрик и интеграцию с различными системами мониторинга.
- * </p>
- *
- * @author gualsh
- * @version 1.0
- */
 @SpringBootApplication
 @EnableScheduling  // Включаем планирование для демонстрационных задач
 public class MicrometerDemoApplication {
 
-    /**
-     * Точка входа в приложение.
-     *
-     * @param args аргументы командной строки
-     */
+    // todo Смотрим README.md
     public static void main(String[] args) {
         SpringApplication.run(MicrometerDemoApplication.class, args);
     }
