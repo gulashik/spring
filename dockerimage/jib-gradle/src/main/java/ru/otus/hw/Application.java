97fcb2d8edd87c1384182335be9de6ea5cbc8cb0
Максим Гулько <gulashmail@gmail.com>
2025-06-04 19:55:43 +0300

    working state

--- dockerimage/jib-gradle/src/main/java/ru/otus/hw/Application.java
+++ dockerimage/jib-gradle/src/main/java/ru/otus/hw/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+        // Запустить ACTIONS.md
         SpringApplication.run(Application.class, args);
     }
 }
